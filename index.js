289378d2903c94fcc17e8a900d5c3f697a92938e
Gowthaman Aravindan <gowthaman169@gmail.com>
2022-03-16 10:43:16 +0800

    Added index page and CSS
    Added blackjack table background

---
+++ index.js
@@ -0,0 +1,8 @@
+let cardNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+let firstCard = cardNumbers[Math.floor(Math.random() * 9)];
+let secondCard = cardNumbers[Math.floor(Math.random() * 9)];
+let sum = firstCard + secondCard;
+
+const startGame = () => {
+    
+}
